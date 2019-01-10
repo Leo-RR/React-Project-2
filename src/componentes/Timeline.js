a4bf418a5454e0eca5e27edfdfaf9905ad0d917b
Leonardo <leo_ors@hotmail.com>
2019-01-10 02:12:52 -0200

    chamando api e passando dados para componente

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import Foto from './Foto';
+import FotoItem from './Foto';
 
 export default class Timeline extends Component {
+
+    constructor(){
+        super();
+        this.state = {fotos:[]};
+    }
+    
+    componentDidMount(){
+        fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+        .then(res => res.json())
+        .then(response => {this.setState({fotos:response}); console.log(response)})
+        .catch(err => console.log(err));
+    }
+
     render(){
         return (
         <div className="fotos container">
-          <Foto/>
-          <Foto/>
+          {
+              this.state.fotos.map(foto => <FotoItem foto={foto}/>)
+          }
         </div>            
         );
     }
