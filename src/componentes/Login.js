309bfcb3d11dfe5a0ce62cb4ad1ec79e64035e7e
Leonardo <leo_ors@hotmail.com>
2019-01-13 23:25:50 -0200

    Chamada para API de login

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 
 export default class Login extends Component{
 
+    envia(event){
+        event.preventDefault();
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({'login': this.login.value, 'senha': this.senha.value}),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch('https://instalura-api.herokuapp.com/api/public/login', requestInfo)
+        .then()
+    }
+
     render(){
 
         return(
             <div className="login-box">
                <h1 className="header-logo">Instalura</h1>
-               <form> 
-                    <input type="text" />
-                    <input type="password"/>
+               <form onSubmit={this.envia.bind(this)}> 
+                    <input type="text" ref={input => this.login = input}/>
+                    <input type="password" ref={input => this.senha = input}/>
                     <input type="submit" value="login"/>
                 </form>
             </div>
